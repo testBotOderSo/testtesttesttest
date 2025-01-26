464aad244ce94e1d096904ca9bb30e767ed4cb09
NotedBot <tvwydios@gmail.com>
2025-01-26 19:46:30 +0100

    Update language.js

--- ressourcen/language.js
+++ ressourcen/language.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const deButton = document.getElementById('de-button');
     const usButton = document.getElementById('us-button');
-    const titleElement = document.getElementById('title');
     const searchInput = document.getElementById('search-input');
 
     const switchToGerman = () => {
-        titleElement.textContent = '<img src="img/notedbotlogo.png" alt="NotedBot Logo">NotedBot Commands';
         searchInput.placeholder = '🔎 Suchen Sie nach einem Command?';
     };
 
     const switchToEnglish = () => {
-        titleElement.textContent = '<img src="img/notedbotlogo.png" alt="NotedBot Logo">NotedBot Commands';
         searchInput.placeholder = '🔎 Search for a command?';
     };
 
