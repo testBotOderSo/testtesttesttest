7197c86a633b84f6b49a15dee03055c454ccdcc8
NotedBot <tvwydios@gmail.com>
2025-01-27 21:02:26 +0100

    Update language.js

--- ressourcen/language.js
+++ ressourcen/language.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const deButton = document.getElementById('de-button');
     const usButton = document.getElementById('us-button');
     const searchInput = document.getElementById('search-input');
-
+    const categoryFilter = document.getElementById('category-filter');
+    const permissionFilter = document.getElementById('permission-filter');
+    // switchToGerman
     const switchToGerman = () => {
         searchInput.placeholder = '🔎 Suchen Sie nach einem Command?';
+        categoryFilter.placeholder = 'Kategorie';
+        permissionFilter.placeholder = 'Berechtigung';
     };
-
+    // switchToEnglish
     const switchToEnglish = () => {
         searchInput.placeholder = '🔎 Search for a command?';
+        categoryFilter.placeholder = 'Category';
+        permissionFilter.placeholder = 'Permission';
     };
 
     deButton.addEventListener('click', switchToGerman);
