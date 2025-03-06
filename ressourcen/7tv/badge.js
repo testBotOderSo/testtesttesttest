577a33a58bde83775aa3857bad45f90749f07a3c
NotedBot <tvwydios@gmail.com>
2025-03-06 12:37:39 +0100

    Update badge.js

--- ressourcen/7tv/badge.js
+++ ressourcen/7tv/badge.js
@@ -1,4 +1,22 @@
-import { fetchTwitchColor } from '../twitch/color.js';
+async function fetchTwitchColor(username) {
+    try {
+        const response = await fetch(`https://api.ivr.fi/v2/twitch/user?login=${username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        if (!response.ok) {
+            console.error(`Fehler beim Fetchen von Twitch Farbe: ${response.status}`);
+            return null;
+        }
+        const data = await response.json();
+        return data[0]?.chatColor || '#FFFFFF';
+    } catch (error) {
+        console.error(`Fehler: ${error}`);
+        return '#FFFFFF';
+    }
+}
 
 function getUrlParams() {
     const urlParams = new URLSearchParams(window.location.search);
