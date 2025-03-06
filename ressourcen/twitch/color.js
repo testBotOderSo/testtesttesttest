f69fad3679937fefc4c0e7b81c6930d92dcb3b6a
NotedBot <tvwydios@gmail.com>
2025-03-06 12:16:03 +0100

    Add files via upload

---
+++ ressourcen/twitch/color.js
@@ -0,0 +1,19 @@
+async function fetchTwitchColor(username) {
+    try {
+        const response = await fetch(`https://api.ivr.fi/v2/twitch/user?login=${username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        if (!response.ok) {
+            console.error(`Fehler beim fetchen von Twtich Farbe: ${response.status}`);
+            return null;
+        }
+        const data = await response.json();
+        return data[0]?.chatColor || '#FFFFFF';
+    } catch (error) {
+        console.error(`Fehler: ${error}`);
+        return '#FFFFFF';
+    }
+}
