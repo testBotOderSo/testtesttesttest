280171a985c813078a8cb8230055a41ac3ad6115
NotedBot <tvwydios@gmail.com>
2025-03-07 18:44:59 +0100

    Update paint.js

--- ressourcen/7tv/paint.js
+++ ressourcen/7tv/paint.js
@@ -54,53 +54,15 @@ function loadPaint() {
 
     function downloadPaintImage() {
         const { elementID, paintID, paintName } = getUrlParams();
+        const paintUrl = `https://cdn.7tv.app/paint/${elementID}/layer/${paintID}/1x.webp`;
     
-        if (elementID && paintID) {
-            const paintUrl = `https://cdn.7tv.app/paint/${elementID}/layer/${paintID}/1x.webp`;
-    
-            const img = new Image();
-            img.crossOrigin = 'anonymous';
-            img.src = paintUrl;
-    
-            img.onload = function () {
-                const gif = new GIF({
-                    workers: 2,
-                    quality: 10
-                });
-    
-                const canvas = document.createElement('canvas');
-                const ctx = canvas.getContext('2d');
-                canvas.width = img.width;
-                canvas.height = img.height;
-
-                ctx.drawImage(img, 0, 0);
-
-                if (paintName) {
-                    ctx.font = 'bold 60px Arial';
-                    ctx.fillStyle = 'white';
-                    ctx.textAlign = 'center';
-                    ctx.fillText(paintName, canvas.width / 2, canvas.height / 2);
-                }
-
-                gif.addFrame(canvas, { delay: 200, copy: true });
-                
-                gif.on('finished', function(blob) {
-                    const link = document.createElement('a');
-                    link.href = URL.createObjectURL(blob);
-                    link.download = `PaintColor-${paintName}.gif`; 
-                    link.click();
-                });
-
-                gif.render();
-            };
-    
-            img.onerror = function () {
-                alert('Fehler beim Laden des Bildes');
-            };
-        }
+        const link = document.createElement('a');
+        link.href = paintUrl;
+        link.download = `${paintName}.webp`;
+        link.click();
     }
-    const downloadBtn = document.getElementById('download-btn').style.display = 'block';
-    downloadBtn.addEventListener('click', downloadPaintImage);
+    
+    document.getElementById('download-btn').addEventListener('click', downloadPaintImage);
 };
 
 window.onload = loadPaint;
